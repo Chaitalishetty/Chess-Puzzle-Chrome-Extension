edbfb3a891bfe5f4a35f125952c93a5fc6b9b684
Chaitalish02 <66556168+Chaitalishetty@users.noreply.github.com>
2022-01-10 17:00:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// axios.get("https://api.chess.com/pub/puzzle")
+// .then(response=>{
+//     var board=document.getElementById("board");
+//     console.log(response.data.title);
+//     board.innerHTML=response.data.fen;
+// })
